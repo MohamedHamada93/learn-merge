858b80b7cb73f3d5a10557b18cd5d3c525acb4be
Mohamed Hamada <mohamed.hamada0103@gmail.com>
2021-01-05 16:10:29 +0200

    hahahaha2

--- Report.php
+++ Report.php
@@ -16,6 +16,11 @@ class Report extends Model
 		return $this->belongsTo('App\User','user_id','id');
 		return $this->belongsTo('App\User','user_id','id');
 		return 'hahahahahahahahahaha';
+		return 'hahahahahahahahahaha';
+		return 'hahahahahahahahahaha';
+		return 'hahahahahahahahahaha';
+		return 'hahahahahahahahahaha';
+		return 'hahahahahahahahahaha';
 	}
 }
 
