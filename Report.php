6fbd9c13d78f941d0c76f5ac2e473171711311ce
Mohamed Hamada <mohamed.hamada0103@gmail.com>
2021-01-05 15:56:13 +0200

    repet

--- Report.php
+++ Report.php
@@ -12,6 +12,17 @@ class Report extends Model
 	{
 		return $this->belongsTo('App\User','user_id','id');
 		return $this->belongsTo('App\User','user_id','id');
+		return $this->belongsTo('App\User','user_id','id');
+		return $this->belongsTo('App\User','user_id','id');
+		return $this->belongsTo('App\User','user_id','id');
+		return $this->belongsTo('App\User','user_id','id');
+		return $this->belongsTo('App\User','user_id','id');
+		return $this->belongsTo('App\User','user_id','id');
+		return $this->belongsTo('App\User','user_id','id');
+		return $this->belongsTo('App\User','user_id','id');
+		return $this->belongsTo('App\User','user_id','id');
+		return $this->belongsTo('App\User','user_id','id');
+		return $this->belongsTo('App\User','user_id','id');
 	}
 }
 
